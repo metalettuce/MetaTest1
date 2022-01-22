aabfeef3e20b6150a40e8b432d2e44dbea4d06d8
metalettuce <metalettuce@protonmail.com>
2022-01-22 08:44:10 -0800

    Initial Commit

---
+++ Shared/MetaTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  MetaTest1App.swift
+//  Shared
+//
+//  Created by metalettuce on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct MetaTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
